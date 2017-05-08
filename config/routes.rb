1457db9d2142698e094ba36181712d00ff12faca
Juliano Maia <leite.juliano@gmail.com>
2017-05-08 04:35:22 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root 'welcome#index'
+  get  '/cadastro', to: 'users#new'
+  post '/cadastro',  to: 'users#create'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
