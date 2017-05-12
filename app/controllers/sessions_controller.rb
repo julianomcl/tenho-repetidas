d63b2818fe4eb1bcb4ee503d8e9b06e13389fc23
Juliano Maia <leite.juliano@gmail.com>
2017-05-12 23:14:50 +0000

    correcoes, traducoes e remocao de necessidade de ativacao da conta

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
+      #if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
-      else
-        message  = "Conta não ativada."
-        message += "Verifique seu e-mail com o link de ativação."
-        flash[:warning] = message
-        redirect_to root_url
-      end
+      #else
+      #  message  = "Conta não ativada."
+      #  message += "Verifique seu e-mail com o link de ativação."
+      #  flash[:warning] = message
+      #  redirect_to root_url
+      #end
     else
       flash.now[:danger] = 'E-mail ou senha inválidos.'
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    #@users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.all.paginate(page: params[:page])
   end
   
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
+    #redirect_to root_url and return unless FILL_IN
   end
   
   def new
