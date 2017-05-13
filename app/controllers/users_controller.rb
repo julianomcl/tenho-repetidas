29f8cb1833a95169061f7d0eb01879dac77e45d4
Caio Granero <caio.granero@usp.br>
2017-05-13 12:08:20 -0300

    forget one detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :birthday, :address, :mobile, :facebook, :twitter, :telegram, :skype)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :birthday, :address, :mobile, :facebook, :twitter, :telegram)
     end
 
     # Before filters
