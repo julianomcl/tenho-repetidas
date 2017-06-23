72ea94f7b0d6c6c806363c021746fe90c0b7ae04
Bruno Feiteiro <bmfeiteiro@gmail.com>
2017-06-23 02:14:37 +0000

    inclusao tabela figurinhas

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,14 @@ ActiveRecord::Schema.define(version: 20170620193516) do
     t.index ["album_id"], name: "index_albums_users_on_album_id"
     t.index ["user_id"], name: "index_albums_users_on_user_id"
   end
-
+ 
+  create_table "figurinhas", force: :cascade do |t|
+  t.integer "figurinha_id"
+  t.integer "album_id"
+  t.index ["figurinha_id"], name: "index_figurinhas_on_figurinha_id"
+  t.index ["album_id"], name: "index_albums_users_on_figurinha_id"
+  end
+  
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -38,7 +45,6 @@ ActiveRecord::Schema.define(version: 20170620193516) do
     t.string   "facebook"
     t.string   "twitter"
     t.string   "telegram"
-    t.string   "skype"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.string   "password_digest"
