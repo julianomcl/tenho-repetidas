37b89231aa4cd37362d1e53c8a875013efd7517e
Juliano Maia <leite.juliano@gmail.com>
2017-06-28 21:23:15 +0000

    Revert "inclusao tabela figurinhas"
    This reverts commit 72ea94f7b0d6c6c806363c021746fe90c0b7ae04.

--- db/schema.rb
+++ db/schema.rb
@@ -28,14 +28,7 @@ ActiveRecord::Schema.define(version: 20170620193516) do
     t.index ["album_id"], name: "index_albums_users_on_album_id"
     t.index ["user_id"], name: "index_albums_users_on_user_id"
   end
- 
-  create_table "figurinhas", force: :cascade do |t|
-  t.integer "figurinha_id"
-  t.integer "album_id"
-  t.index ["figurinha_id"], name: "index_figurinhas_on_figurinha_id"
-  t.index ["album_id"], name: "index_albums_users_on_figurinha_id"
-  end
-  
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -45,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170620193516) do
     t.string   "facebook"
     t.string   "twitter"
     t.string   "telegram"
+    t.string   "skype"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.string   "password_digest"
